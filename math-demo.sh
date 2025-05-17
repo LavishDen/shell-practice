70129e6332b8de3e5a182f4a4a31363300acf5f0
Lavish <117949210+Lavish-Lavish@users.noreply.github.com>
2025-05-17 05:53:32 -0400

    Add math-demo.sh, strings.sh, array-demo.sh

---
+++ math-demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This sctipt performs basic arithmetic operations
+a=10
+b=5
+echo "First argumemt $a"
+echo "Second argument $b"
+echo "a=$a and b=$b"
+echo "Sum: $((a+b))"
+echo "Difernece: $((a-b))"
+echo "Product: $((a*b))"
+echo "Division: $((a/b))"
+echo "Modulus: $((a%b))"
