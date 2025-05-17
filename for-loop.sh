0d3d2fde6436598afe5cc13b2f47dcef54792123
Lavish <117949210+Lavish-Lavish@users.noreply.github.com>
2025-05-17 03:13:32 -0400

    Add for-loop.sh: print numbers 1 to 5 using a for loop

---
+++ for-loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script prints numbers from 1 to 5 using for loop
+for i in 1 2 3 4 5 
+do
+echo " Number: $i"
+done
+
+
+# for i in 1 2 3 4 5 -> loop throuh values 1 to 5
+# do .. done -> loop body -> commands to execute each time
+# $i -> current value of the loop variable i
